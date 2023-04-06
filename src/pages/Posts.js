e9997e0f2606f721909295aa5c5efefc34f99a42
nhokcherry <baosuper456@gmail.com>
2023-04-06 10:28:17 +0700

    try #8

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -32,8 +32,8 @@ const Posts = () => {
     return ( 
         <div className="container-page" style={{backgroundImage: `url(https://wallpaperset.com/w/full/9/f/b/38639.jpg)`, backgroundRepeat: 'no-repeat', backgroundSize:'cover'}}>
             <div className="custom-post">
-                <div className="row" style={{marginTop: '30px'}}>
-                    {post && post.map((data)=>(
+                {post && post.map((data)=>(
+                    <div className="row" style={{marginTop: '30px'}}>
                         <div className="col-sm-3" style={{width: '300px', marginTop: '30px'}}>
                             <Link to={`/details/${data._id}`} key={data._id}>
                                 <MDBCard>
@@ -48,8 +48,8 @@ const Posts = () => {
                                 </MDBCard>
                             </Link>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
