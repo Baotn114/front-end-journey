5d4cf2728264d0e5ed1781ae14872335e567d7ad
nhokcherry <baosuper456@gmail.com>
2023-05-05 11:10:04 +0700

    fix loading screen - posts

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -33,29 +33,40 @@ const Posts = () => {
     return ( 
         <div className="container-page" style={{backgroundImage: `url(https://wallpaperset.com/w/full/9/f/b/38639.jpg)`, backgroundRepeat: 'no-repeat', backgroundSize:'cover'}}>
             <div className="custom-post">
-            {!post
-                    ?   <div className="row" style={{marginTop: '30px'}}>
-                            <p>Please wait a minute to let the back-end api work probably</p>
+                <div className="row" style={{marginTop: '30px'}}>
+                    {!post 
+                    
+                    ?   <div className='custom-post' style={{marginTop: '20%'}}>
+                            <MDBSpinner grow className='mx-2' size="sm" color='info'>
+                                <span className=''></span>
+                            </MDBSpinner>
+                            <MDBSpinner grow className='mx-2' size="sm" color='info'>
+                                <span className=''></span>
+                            </MDBSpinner>  
+                            <MDBSpinner grow className='mx-2' size="sm" color='info'>
+                                <span className=''></span>
+                            </MDBSpinner>
+                            <MDBSpinner grow className='mx-2' size="sm" color='info'>
+                                <span className=''></span>
+                            </MDBSpinner>
+                        </div>          
+                    : post.map((data)=>(
+                        <div className="col-sm-3" style={{width: '300px', marginTop: '30px'}}>
+                            <Link to={`/details/${data._id}`} key={data._id}>
+                                <MDBCard>
+                                    <MDBCardImage src={data.image} alt='...' position='top' />
+                                    <MDBCardBody>
+                                    <MDBCardTitle>{data.title}</MDBCardTitle>
+                                    <MDBCardText><strong>Name: {data.userName}</strong></MDBCardText>
+                                    <MDBCardText>
+                                        <small className='text-muted'>{formatDistanceToNow(new Date(data.createdAt), {addSuffix: true})}</small>
+                                    </MDBCardText>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </Link>
                         </div>
-                    :   <div className="row" style={{marginTop: '30px'}}>
-                            {post && post.map((data)=>(
-                                <div className="col-sm-3" style={{width: '300px', marginTop: '30px'}}>
-                                    <Link to={`/details/${data._id}`} key={data._id}>
-                                        <MDBCard>
-                                            <MDBCardImage src={data.image} alt='...' position='top' />
-                                            <MDBCardBody>
-                                            <MDBCardTitle>{data.title}</MDBCardTitle>
-                                            <MDBCardText><strong>Name: {data.userName}</strong></MDBCardText>
-                                            <MDBCardText>
-                                                <small className='text-muted'>{formatDistanceToNow(new Date(data.createdAt), {addSuffix: true})}</small>
-                                            </MDBCardText>
-                                            </MDBCardBody>
-                                        </MDBCard>
-                                    </Link>
-                                </div>
-                            ))}
-                        </div>
-            }
+                    ))}
+                </div>
             </div>
         </div>
     );
